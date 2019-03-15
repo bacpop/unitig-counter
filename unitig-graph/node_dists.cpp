885aa1189bbd979ef65a4e88c51d54437d40e30c
John Lees <lees.john6@gmail.com>
2019-03-15 15:53:58 -0400

    Add C++ for distance between nodes

---
+++ unitig-graph/node_dists.cpp
@@ -0,0 +1,128 @@
+/*
+ * node_dists.cpp
+ * Class to calculate distances between graph nodes
+ *
+ * Some code modified from DBGWAS by Jacob L., Jaillard M., Lima L.
+ * Please cite:
+ *  Magali Jaillard, Maud Tournoud, Leandro Lima, Vincent Lacroix,
+ *  Jean-Baptiste Veyrieras and Laurent Jacob, "Representing Genetic
+ *  Determinants in Bacterial GWAS with Compacted De Bruijn Graphs", 2017,
+ *  Cold Spring Harbor Labs Journals, doi:10.1101/113563.
+ *  (url: http://www.biorxiv.org/content/early/2017/03/03/113563)
+ *
+ *  This code: John Lees
+ *
+ */
+
+#include "node_dists.hpp"
+
+// Graph initialisation
+Cdbg::Cdbg(const string& dbgPrefix)
+   : Cdbg(dbgPrefix + ".nodes", dbgPrefix + ".edges.dbg")
+{
+}
+
+Cdbg::Cdbg(const string& nodeFile, const string& edgeFile)
+{
+    int nbContigs = getNbLinesInFile(nodeFile);
+    _dbgGraph = graph_t(nbContigs);
+
+    // Create the nodes
+    ifstream nodeIst(nodeFile.c_str());
+    if (!nodeIst)
+    {
+        throw std::runtime_error("Could not open node file " + nodeFile + "\n");
+    }
+
+    int id;
+    string sequence;
+    while (nodeIst >> id >> sequence)
+    {
+        MyVertex vF = vertex(id, _dbgGraph);
+        _dbgGraph[vF].name = sequence;
+        _dbgGraph[vF].length = sequence.length();
+        _dbgGraph[vF].id = id;
+        _seqs[sequence] = id;
+    }
+
+    nodeIst.close();
+
+    // Create the edges
+    ifstream edgeIst(edgeFile.c_str());
+    if (!edgeIst)
+    {
+        throw std::runtime_error("Could not open edge file " + edgeFile + "\n");
+    }
+
+    int from, to;
+    int index = 0;
+    string label;
+    while (edgeIst >> from >> to >> label)
+    {
+        MyVertex fromVertex = vertex(from, _dbgGraph);
+        MyVertex toVertex = vertex(to, _dbgGraph);
+
+        // Add from -> to
+        pair<MyEdge, bool> return_from_add_edge = add_edge(fromVertex,
+                                                           toVertex,
+                                                           _dbgGraph);
+        if (return_from_add_edge.second) {
+            _dbgGraph[return_from_add_edge.first].id = index;
+            _dbgGraph[return_from_add_edge.first].weight = _dbgGraph[fromVertex].length;
+            index++;
+        }
+
+        // Add to -> from
+        pair<MyEdge, bool> return_from_add_edge = add_edge(toVertex,
+                                                           fromVertex,
+                                                           _dbgGraph);
+        if (return_from_add_edge.second) {
+            _dbgGraph[return_from_add_edge.first].id = index;
+            _dbgGraph[return_from_add_edge.first].weight = _dbgGraph[toVertex].length;
+            index++;
+        }
+    }
+    edgeIst.close();
+}
+
+// Graph alogorithms and helper functions
+
+vector<int> Cdbg::node_distance(const string& origin)
+{
+    vector<int> distances(num_vertices(_dbgGraph));
+    fill(distances.begin(), distances.end(), 0);
+
+    MyVertex originVertex = vertex(_seqs[origin], _dbgGraph);
+    dijkstra_shortest_paths(_dbgGraph, originVertex,
+                            distance_map(boost::make_iterator_property_map(distances.begin(),
+                                         boost::get(boost::vertex_index, _dbgGraph))));
+
+    return(distances);
+}
+
+long int getNbLinesInFile(const string &filename) {
+    ifstream ist(filename.c_str());
+    if (!ist)
+    {
+        throw std::runtime_error("Could not open file " + filename + "\n");
+    }
+
+    // Number of lines in the file
+    long int n = std::count(std::istreambuf_iterator<char>(ist), std::istreambuf_iterator<char>(), '\n');
+
+    ist.close();
+
+    return n;
+}
+
+int main (int argc, char *argv[])
+{
+    Cdbg exampleGraph = Cdbg("../test_data/unitigs");
+
+    // node id 22
+    vector<int> dists = exampleGraph.node_distance("GTAATAAACAAAAAAAAAAGTTAAAAATTAAATAAAAATACTTGACTAAATAAAATATACCTGTTAGAATAAAAACAAGGAAAAAGAAAG");
+    // dist to 23
+    cout << dists[exampleGraph.get_vertex("AAAAAAAAAAGTTAAAAATTACATAAAAATA")] << endl;
+
+    return 0;
+}
---
+++ unitig-graph/node_dists.hpp
@@ -0,0 +1,76 @@
+/*
+ * node_dists.hpp
+ * Class header to calculate distances between graph nodes
+ *
+ * Some code modified from DBGWAS by Jacob L., Jaillard M., Lima L.
+ * Please cite:
+ *  Magali Jaillard, Maud Tournoud, Leandro Lima, Vincent Lacroix,
+ *  Jean-Baptiste Veyrieras and Laurent Jacob, "Representing Genetic
+ *  Determinants in Bacterial GWAS with Compacted De Bruijn Graphs", 2017,
+ *  Cold Spring Harbor Labs Journals, doi:10.1101/113563.
+ *  (url: http://www.biorxiv.org/content/early/2017/03/03/113563)
+ *
+ *  This code: John Lees
+ *
+ */
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <algorithm>
+
+#include <boost/config.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/property_map/property_map.hpp>
+#include <boost/graph/properties.hpp>
+#include <boost/graph/labeled_graph.hpp>
+#include <boost/graph/filtered_graph.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+#include <boost/graph/subgraph.hpp>
+#include <boost/graph/reverse_graph.hpp>
+#include <boost/graph/graph_utility.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+
+using namespace std;
+
+//vertex informations
+struct VertexInfo {
+    string name;
+    int length;
+    int id;
+};
+
+//edge informations
+struct EdgeInfo {
+    int id;
+    int weight;
+};
+
+// Use recommended typedefs
+typedef boost::property<boost::vertex_index_t, std::size_t, VertexInfo> vertex_prop;
+typedef boost::property<boost::edge_index_t, std::size_t, EdgeInfo> edge_prop;
+typedef boost::adjacency_list <boost::setS, boost::vecS, boost::directedS, vertex_prop, edge_prop> adjlist_t;
+typedef boost::subgraph< adjlist_t > graph_t;
+typedef boost::graph_traits<adjlist_t>::vertex_descriptor MyVertex;
+typedef boost::graph_traits<adjlist_t>::edge_descriptor MyEdge;
+
+class Cdbg
+{
+   public:
+        // Initialisation
+        Cdbg(const string& dbgPrefix);
+        Cdbg(const string& nodeFile, const string& edgeFile);
+
+        // Non-modifying operations
+        MyVertex get_vertex(const string& sequence) { return vertex(_seqs[sequence], _dbgGraph); }
+
+        // Modifying operations
+        vector<int> node_distance(const string& origin);
+
+   protected:
+      graph_t _dbgGraph;
+      unordered_map<string, int> _seqs;
+};
+
+long int getNbLinesInFile(const string &filename);
